db267e4e8f0c86567898f7d1e905edd2252b286d
Gilmar Munoz <gialmuno@espol.edu.ec>
2025-11-12 03:37:40 -0500

    Adición de nuevos tokens por Gilmar Muñoz
    Se han agregado más palabras reservadas y tokens correspondientes a DART

--- Analizador-Lex-Dart.py
+++ Analizador-Lex-Dart.py
@@ -13,7 +13,28 @@ reserved = {
             "float": "FLOAT",   #lisbllam
             "bool": "BOOL",     #lisbllam
             "const": "CONST",   #lisbllam
-            "final": "FINAL"    #lisbllam
+            "final": "FINAL",   #lisbllam
+            "null": "NULL", #gilmaramg66
+            "true": "TRUE", #gilmaramg66
+            "false": "FASLSE", #gilmaramg66
+            "break": "BREAK", #gilmaramg66
+            "void": "VOID", #gilmaramg66
+            "try": "TRY", #gilmaramg66
+            "catch": "CATCH", #gilmaramg66
+            "throw": "THROW", #gilmaramg66
+            "assert": "ASSERT", #gilmaramg66
+            "static": "STATIC", #gilmaramg66
+            "abstract": "ABSTRACT", #gilmaramg66
+            "interface": "INTERFACE", #gilmaramg66
+            "extends": "EXTENDS", #gilmaramg66
+            "implements": "IMPLEMENTS", #gilmaramg66
+            "super": "SUPER", #gilmaramg66
+            "as": "AS", #gilmaramg66
+            "set": "SET", #gilmaramg66
+            "import": "IMPORT", #gilmaramg66
+            "library": "LIBRARY", #gilmaramg66
+            "do": "DO" #gilmaramg66
+            
 }
 # List of token names.   This is always required
 tokens = (
@@ -40,6 +61,18 @@ tokens = (
    'COMENTARIO',   #anzagood1
    'NULLABLE',
    'METODO',  #anzagood1
+   'LAMBDA', #gilmaramg66
+   'HASH', #gilmaramg66
+   'COLON', #gilmaramg66 
+   'NOT', #gilmaramg66
+   'COMMENTBLOCK', #gilmaramg66
+   'DOLLAR', #gilmaramg66
+   'BIT_AND', #gilmaramg66
+   'BIT_OR', #gilmaramg66
+   'BIT_XOR', #gilmaramg66
+   'BIT_NOT', #gilmaramg66
+   'LSHIFT', #gilmaramg66
+   'RSHIFT' #gilmaramg66
 ) + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
@@ -62,6 +95,17 @@ t_AND = r'&&'
 t_OR = r'\|\|'
 t_SEMICOLON = r';' #anzagood
 t_NULLABLE = r'\?' #lisbllam
+t_LAMBDA = r'=>' #gilmaramg66
+t_HASH = r'\#' #gilmaramg66
+t_COLON = r':' #gilmaramg66
+t_NOT = r'!' #gilmaramg66
+t_DOLLAR = r'\$' #gilmaramg66
+t_BIT_AND = r'&' #gilmaramg66
+t_BIT_OR = r'\|' #gilmaramg66
+t_BIT_NOT = r'~' #gilmaramg66
+t_BIT_XOR = r'\^' #gilmaramg66
+t_LSHIFT = r'<<' #gilmaramg66
+t_RSHIFT = r'>>' #gilmaramg66
 
 
 def t_CADENA(t):    #lisbllam
@@ -82,6 +126,10 @@ def t_ID(t):        #anzagood1
     r'[a-zA-Z][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, 'ID')
     return t
+
+def t_COMMENTBLOCK(t): #gilmaramg66
+    r'/\*(.|\n)*?\*/'
+    pass
 
 # Define a rule so we can track line numbers
 def t_newline(t):
